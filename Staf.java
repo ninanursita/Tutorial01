49897a8fb08ca0394f19a35a2b4089b82e7de433
ninanursita99 <ninanursita99@gmail.com>
2018-09-07 15:39:32 +0700

    Second commit Staf.java

---
+++ Staf.java
@@ -0,0 +1,10 @@
+
+public class Staf extends Elemen {
+	private String title;
+
+	public Staf(String name, int age, String gender, String title) {
+		super(name, age, gender);
+		this.title = title;
+	}
+	
+}
